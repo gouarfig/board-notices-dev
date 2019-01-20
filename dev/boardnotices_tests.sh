e6492eb96e02a55afefa87c0e466c4bba88e4ab4
FredQ <gouarfig@yahoo.fr>
2019-01-20 23:15:53 +0000

    Exclude effing .git

--- dev/boardnotices_tests.sh
+++ dev/boardnotices_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ..
-rsync -avz --exclude "*/.git/*"--delete boardnotices phpbb/phpBB/ext/fq/
+rsync -avz --exclude "*/.git/*" --delete boardnotices phpbb/phpBB/ext/fq/
 cd phpbb
 phpBB/vendor/bin/phpunit --configuration phpBB/ext/fq/boardnotices/phpunit.xml.dist --bootstrap tests/bootstrap.php $@
 
