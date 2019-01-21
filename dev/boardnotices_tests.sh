a6eb85e4dc7cb6785050f3186d6cd83b223ac082
FredQ <gouarfig@yahoo.fr>
2019-01-21 15:32:18 +0000

    Fix local tests

--- dev/boardnotices_tests.sh
+++ dev/boardnotices_tests.sh
@@ -2,6 +2,11 @@
 
 cd ..
 rsync -avz --exclude "*/.git/*" --delete boardnotices phpbb/phpBB/ext/fq/
+epv/src/EPV.php run --dir="phpbb/phpBB/ext/fq/boardnotices/"
 cd phpbb
-phpBB/vendor/bin/phpunit --configuration phpBB/ext/fq/boardnotices/phpunit.xml.dist --bootstrap tests/bootstrap.php $@
+phpBB/vendor/bin/phpunit \
+  --configuration phpBB/ext/fq/boardnotices/phpunit.xml.dist \
+  --bootstrap tests/bootstrap.php \
+  --testsuite "Extension Test Suite" \
+  $@
 
