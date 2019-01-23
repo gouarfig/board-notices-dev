fd8990a0acf38a391f811db36d00eda0f3974aaa
FredQ <gouarfig@yahoo.fr>
2019-01-23 11:48:33 +0000

    Copy new files to quickinstall env

--- dev/boardnotices_tests.sh
+++ dev/boardnotices_tests.sh
@@ -2,6 +2,7 @@
 
 cd ..
 rsync -avz --exclude "*/.git/*" --delete boardnotices phpbb/phpBB/ext/fq/
+rsync -avz --exclude "*/.git/*" --delete boardnotices ../quickinstall/quickinstall/boards/phpbb32/ext/fq/
 epv/src/EPV.php run --dir="phpbb/phpBB/ext/fq/boardnotices/"
 cd phpbb
 phpBB/vendor/bin/phpunit \
