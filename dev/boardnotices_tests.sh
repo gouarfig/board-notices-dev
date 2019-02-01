fe5926969d9bfcf2e69b6d6f7308999586251e9c
FredQ <gouarfig@yahoo.fr>
2019-02-01 12:00:07 +0000

    Avoid deleting code coverage files

--- dev/boardnotices_tests.sh
+++ dev/boardnotices_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cd ..
-rsync -avz --exclude "*/.git/*" --delete boardnotices phpbb/phpBB/ext/fq/
-#rsync -avz --exclude "*/.git/*" --delete boardnotices ../quickinstall/quickinstall/boards/phpbb31/ext/fq/
-rsync -avz --exclude "*/.git/*" --delete boardnotices ../quickinstall/quickinstall/boards/phpbb32/ext/fq/
+rsync -avz --exclude "*/.git/*" --exclude "*/coverage/*" --delete boardnotices phpbb/phpBB/ext/fq/
+#rsync -avz --exclude "*/.git/*" --exclude "*/coverage/*" --delete boardnotices ../quickinstall/quickinstall/boards/phpbb31/ext/fq/
+rsync -avz --exclude "*/.git/*" --exclude "*/coverage/*" --delete boardnotices ../quickinstall/quickinstall/boards/phpbb32/ext/fq/
 epv/src/EPV.php run --dir="phpbb/phpBB/ext/fq/boardnotices/"
 cd phpbb
 phpBB/vendor/bin/phpunit \
