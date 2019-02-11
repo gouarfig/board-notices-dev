81cdac0342142976d0c2ed6754e22414d3b262a9
FredQ <gouarfig@yahoo.fr>
2019-02-11 16:52:03 +0000

    Adding debug

--- dev/vstests.sh
+++ dev/vstests.sh
@@ -5,7 +5,8 @@ rsync -avz --exclude "*/.git/*" --exclude "*/coverage/*" --delete boardnotices p
 cd phpbb
 echo Starting phpunit from
 pwd
-php -c /etc/php.ini.default phpBB/vendor/bin/phpunit \
+export XDEBUG_CONFIG="idekey=VSCODE"
+php phpBB/vendor/bin/phpunit \
   --configuration phpBB/ext/fq/boardnotices/phpunit.xml \
   --bootstrap tests/bootstrap.php \
   --debug --verbose \
