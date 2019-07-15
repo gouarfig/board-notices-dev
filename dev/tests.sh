3f4295fc51bda040c88d06878c682b86acb5ba63
FredQ <gouarfig@yahoo.fr>
2019-07-15 23:13:42 +0100

    Add coverage to local tests

--- dev/tests.sh
+++ dev/tests.sh
@@ -47,7 +47,9 @@ php phpBB/vendor/bin/phpunit \
   --configuration ${extension_path}${extension_name}/phpunit.xml \
   --bootstrap tests/bootstrap.php \
   --testsuite "Extension Test Suite" \
-  --coverage-html ${coverage_folder}
+  --coverage-html ${coverage_folder} \
+  --log-junit ${coverage_folder}/phpunit.xml \
+  --coverage-clover ${coverage_folder}/phpunit.coverage.xml
 
 cd ..
 
